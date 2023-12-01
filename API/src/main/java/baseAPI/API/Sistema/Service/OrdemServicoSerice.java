c6f2bd1b5f779d98cc37d1729f1ea9ffd6b5f56d
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2023-12-01 10:40:22 -0400

    Segundo: Criação de Service Iniciado

---
+++ API/src/main/java/baseAPI/API/Sistema/Service/OrdemServicoSerice.java
@@ -0,0 +1,7 @@
+package baseAPI.API.Sistema.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrdemServicoSerice {
+}
---
+++ API/src/main/java/baseAPI/API/Sistema/Service/UsuarioService.java
@@ -0,0 +1,9 @@
+package baseAPI.API.Sistema.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+
+}
